8d60e2064a204fbf367f6e7dfb0ee1ff48249cd5
gdpepp <dgusti89@gmai.com>
2025-05-26 23:43:14 -0300

    correccion get_lista_tomas

--- src/generators.py
+++ src/generators.py
@@ -3,17 +3,17 @@ import random
 from src import values
 
 mueble_id_counter = 0
-global habitacion
 
-def get_lista_tomas():
-    cantidad_tomas = random.randint(2, 10),
-    lista_tomas = dict
-    for _ in cantidad_tomas:
-        lista_tomas.update({
-            'x': random.randint(0, habitacion["ancho"]),
-            'y': random.randint(0, habitacion["profundidad"])
+
+def get_lista_tomas(ancho, prof):
+    cantidad_tomas = random.randint(2, 10)
+    lista_tomas = []
+    for _ in range(cantidad_tomas):
+        lista_tomas.append({
+            "x": random.randint(0, ancho),
+            "y": random.randint(0, prof)
         })
-
+    return lista_tomas
 
 def get_reglas_adyacencia():
     # TODO reglas_adyacencia: lista de tuplas con nombres de objetos que deben estar juntos.
@@ -21,13 +21,11 @@ def get_reglas_adyacencia():
 
 
 def generar_habitacion():
-    hab = {
-        "ancho": random.randint(200, 1000),
-        "profundidad": random.randint(200, 1000),
-        "tomas": get_lista_tomas(),
-        "reglas_adyacencia": get_reglas_adyacencia()
-    }
-    return hab
+    ancho = random.randint(200, 1000)
+    prof = random.randint(200, 1000)
+    lista_tomas = get_lista_tomas(ancho, prof)
+
+    return {'ancho': ancho, 'profundidad': prof, 'tomas': lista_tomas, 'reglas_adyacencia': get_reglas_adyacencia()}
 
 
 
